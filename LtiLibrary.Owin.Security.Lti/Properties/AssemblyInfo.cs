dc8b6a8e3cab47fd854090c1081b63cc3b8df188
Andy Miller <andy@andyfmiller.com>
2016-10-08 15:40:53 -0700

    Published new version to NuGet.

--- LtiLibrary.Owin.Security.Lti/Properties/AssemblyInfo.cs
+++ LtiLibrary.Owin.Security.Lti/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@
 [assembly: AssemblyDescription("A .NET implementation of OWIN middleware for LTI")]
 [assembly: AssemblyCompany("andyfmiller.com")]
 [assembly: AssemblyProduct("LtiLibrary")]
-[assembly: AssemblyCopyright("Copyright © 2014")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyInformationalVersion("1.0.0-beta")]
